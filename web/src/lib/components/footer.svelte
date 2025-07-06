15a316e7224262e313430fc01043526ffae115a3
joqd <gorgdis@gmail.com>
2025-07-06 16:00:22 +0330

    web boilerplate

---
+++ web/src/lib/components/footer.svelte
@@ -0,0 +1,40 @@
+<footer class="w-full h-[47px] border-t mt-12">
+    <div
+        class="max-w-[640px] mx-auto h-[47px] px-6 sm:px-0 flex items-center justify-between text-sm opacity-75"
+    >
+        <p class="iransans-medium">طراحی شده با ❤️ توسط ابوالفضل</p>
+        <div class="flex items-center gap-2.5">
+            <a
+                href="https://github.com/joqd"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="hover:opacity-100 opacity-75 transition-opacity"
+                ><img
+                    src="/icons/github.svg"
+                    alt="GitHub"
+                    class="w-4 h-4 dark:invert dark:brightness-200"
+                /></a
+            ><a
+                href="https://t.me/joqd_net"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="hover:opacity-100 opacity-75 transition-opacity"
+                ><img
+                    src="/icons/telegram.svg"
+                    alt="Telegram"
+                    class="w-4 h-4 dark:invert dark:brightness-200"
+                /></a
+            ><a
+                href="https://x.com/0xjoqd"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="hover:opacity-100 opacity-75 transition-opacity"
+                ><img
+                    src="/icons/x.svg"
+                    alt="X"
+                    class="w-4 h-4 dark:invert dark:brightness-200"
+                /></a
+            >
+        </div>
+    </div>
+</footer>
